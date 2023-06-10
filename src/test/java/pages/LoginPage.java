4aafd22647d76f57b3725dd1fab70702befd3520
SujathaRia <you@example.com>
2023-06-10 05:50:14 +0000

    saving SimplilearnMaven project

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,49 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import testcases.BaseClass;
+
+public class LoginPage {
+
+	WebDriver pageDriver;
+
+	// ================================objects===================================
+
+	@FindBy(linkText = "Log in")
+	WebElement loginLink;
+
+	@FindBy(name = "user_login")
+	WebElement userName;
+
+	@FindBy(id = "password")
+	WebElement password;
+
+	@FindBy(className = "rememberMe")
+	WebElement RememberMe;
+
+	@FindBy(name = "btn_login")
+	WebElement loginBtn;
+
+	// =====================================Methods==========================================
+
+	public LoginPage(WebDriver driver) {
+		pageDriver = driver;
+		PageFactory.initElements(pageDriver, this);
+	}
+
+	public void LoginFunction(String UserNameVal, String PwdVal) {
+
+		loginLink.click();
+		userName.sendKeys(UserNameVal);
+		password.sendKeys(PwdVal);
+		RememberMe.click();
+		loginBtn.click();
+
+	}
+
+}
